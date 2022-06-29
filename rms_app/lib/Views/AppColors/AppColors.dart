c3fbbbd32f84b35967b866b722cc51ca32fd7c40
Lovestrant Kemboi <71724387+Lovestrant@users.noreply.github.com>
2022-06-29 15:02:08 +0300

    Splash screen

---
+++ rms_app/lib/Views/AppColors/AppColors.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color blueTheme =  Color.fromRGBO(30, 83, 161, 9);
+  static const homeTheme = Color(0xDFE5EF);
+  static const greenishTheme = Color(0x1EA133);
+  static const brownishTheme = Color(0xEC6B6B);
+  static const whitishTheme = Color(0xF5F5F5);
+  static const navyBluishTheme = Color(0x671EA1);
+  static const pinkishTheme = Color(0xEC6B6B);
+  static const orangeTheme = Color(0xF24E1E);
+  static const lightGreenishTheme = Color(0x4ECB71);
+  static const darkTheme = Colors.black;
+  static const purplishTheme = Color(0x671EA1);
+}
---
+++ rms_app/lib/Views/SplashScreen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:rms_app/Views/AppColors/AppColors.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({Key? key, String title = "Splash Screen"})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      color: AppColors.blueTheme,
+      home: Container(
+          margin: const EdgeInsets.symmetric(horizontal: 30, vertical: 30),
+          width: 50,
+          height: 50,
+          color: AppColors.blueTheme,
+          child: const Center(
+            child: Image(image: AssetImage("lib/Assets/RMSLOGO.png")),
+          )),
+    );
+  }
+}
